4f19ba3650c16510478b287fe30f0918798c8e46
Nolan Welch <nolanwelch@outlook.com>
2025-04-19 04:25:58 +0000

    Add development testing kata

---
+++ src/shared/dev-kata.ts
@@ -0,0 +1,6 @@
+import { PublicKata } from "./schema/kata.schema";
+
+export const DEV_KATA: PublicKata = {
+  starterCode: `// Write a function that returns the sum of two numbers\nfunction add(a, b) {\n  return a + b;\n}`,
+  description: "🎉 Dev mode: add two numbers",
+};
