7b784bd9f5a94faa8c2cf478d834652c30bae2b0
Nolan Welch <nolanwelch@outlook.com>
2025-04-19 00:51:39 +0000

    Define Zod kata schema

---
+++ src/shared/schema/kata.schema.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+// Base schema
+export const kataSchema = z.object({
+  starterCode: z.string(),
+  description: z.string().optional(),
+  testCode: z.string(),
+});
+export type Kata = z.infer<typeof kataSchema>;
+
+// Public schema
+export const publicKataSchema = kataSchema.omit({ testCode: true });
+export type PublicKata = z.infer<typeof publicKataSchema>;
+
+// Admin schema
+export const adminKataSchema = kataSchema;
+export type AdminKata = z.infer<typeof adminKataSchema>;
+
+// Create / Update objects
+export const createKataSchema = kataSchema.pick({
+  starterCode: true,
+  description: true,
+  testCode: true,
+});
+export type CreateKataInput = z.infer<typeof createKataSchema>;
+
+export const updateKataSchema = createKataSchema.partial();
+export type UpdateKataInput = z.infer<typeof updateKataSchema>;
