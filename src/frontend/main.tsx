63e31cf0bfba26f22d0a75fb40ecf50fc81fed86
Nolan Welch <nolanwelch@outlook.com>
2025-04-21 00:37:25 +0000

    Simplify app structure

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,10 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
+import AdminPage from "./pages/AdminPage";
+import HomePage from "./pages/HomePage";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:id" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
