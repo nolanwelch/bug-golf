6a7531ad7165310bdc0e5a0a5478529e612d382a
Nolan Welch <nolanwelch@outlook.com>
2025-04-21 00:40:32 +0000

    Add root Clerk auth provider

--- src/frontend/main.tsx
+++ src/frontend/main.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -5,14 +6,18 @@ import "./index.css";
 import AdminPage from "./pages/AdminPage";
 import HomePage from "./pages/HomePage";
 
+const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/:id" element={<HomePage />} />
-        <Route path="/admin" element={<AdminPage />} />
-      </Routes>
+      <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:id" element={<HomePage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </ClerkProvider>
     </BrowserRouter>
   </StrictMode>
 );
